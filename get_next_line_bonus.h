cfb58a4aa6cb8a488364187c032cd13261e932e9
takeshinishikawa <takeshinishikawa@gmail.com>
2021-06-26 19:07:04 -0300

    added bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h> 
+# include <stdlib.h>
+# include <stddef.h>
+# include <fcntl.h>
+# define OPEN_MAX 1024
+
+size_t	ft_strlen(const char*s);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+
+char	*ft_strdup(const char *s1);
+
+size_t	ft_linelen(const char*s);
+
+char	*ft_linedup(const char *s1);
+
+int		get_next_line(int fd, char **line);
+
+#endif
